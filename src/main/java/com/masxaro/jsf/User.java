8e85d338f98bb9ce0ecc28a4baff16aebd721ed9
Brian Shimkaveg <bws@masxaro.com>
2012-07-17 17:02:06 -0400

    updated index page

---
+++ src/main/java/com/masxaro/jsf/User.java
@@ -0,0 +1,26 @@
+package com.masxaro.jsf;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+
+@ManagedBean
+@SessionScoped
+
+public class User {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+	
+}
